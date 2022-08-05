9a9008b1d5790f7ee407abb4fbafa5f48a3c0447
Vivek Gautam <rohit.b@westagilelabs.com>
2022-08-05 20:42:21 +0530

    created geitpl project by django cms

---
+++ golden_eagle/static/include/header.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="keywords" content="">
+  <title>Custom Software Development Company | Golden Eagle IT Technologies</title>
+  <link rel="shortcut icon" href="images/favicon.png" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="css/animate.css" />
+  <link rel="stylesheet" type="text/css" href="css/all.min.css" />
+  <link rel="stylesheet" type="text/css" href="css/fontello.css" />
+  <link rel="stylesheet" type="text/css" href="css/flaticon.css" />
+  <link rel="stylesheet" type="text/css" href="css/themify-icons.css" />
+  <link rel="stylesheet" type="text/css" href="css/slick.css">
+  <link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+  <link rel="stylesheet" type="text/css" href="css/shortcodes.css" />
+  <link rel="stylesheet" type="text/css" href="css/main.css" />
+  <link rel="stylesheet" type="text/css" href="css/responsive.css" />
+  <link rel='stylesheet' id='rs-plugin-settings-css' href="revolution/css/rs6.css">
+</head>
+
+
+<body>
+  
+  <!-- Page Start -->
+  <div class="page">
+    <!-- Header Start -->
+    <header>
+      <div class="Header cmt-stickable-header">
+        <div class="container">
+          <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="index.php">
+              <img height="72" width="180" class="img-fluid auto_size" src="images/logo/logo1.png" alt="logo-img">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    About Us
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                    <li><a class="dropdown-item" href="know-us.php">Know us </a></li>
+                    <li><a class="dropdown-item" href="mission-vision.php">Mission Vision & Goals</a></li>
+                    <li><a class="dropdown-item" href="director-desk.php">Directors Desk</a></li>
+                    <li><a class="dropdown-item" href="teams.php">Our Teams</a></li>
+                    <li><a class="dropdown-item" href="career.php">Careers</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="serviceDropdown" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Services
+                  </a>
+                  <ul class="dropdown-menu large-dropdown service-dropdown" aria-labelledby="serviceDropdown">
+                    <div class="row">
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3><span><img src="images/services/app-development.png" alt="img" width="24"
+                                height="24"></span> App Development</h3>
+                          <ul>
+                            <li><a href="#">App Development</a></li>
+                            <li><a href="#">UI / UX</a></li>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Offshore Development</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3><span><img src="images/services/ui-ux-design.png" alt="img" width="24" height="24"></span>
+                            UI/UX Design</h3>
+                          <ul>
+                            <li><a href="#">App Development</a></li>
+                            <li><a href="#">UI / UX</a></li>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Offshore Development</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3><span><img src="images/services/web-development.png" alt="img" width="24"
+                                height="24"></span> Web Development</h3>
+                          <ul>
+                            <li><a href="#">App Development</a></li>
+                            <li><a href="#">UI / UX</a></li>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Offshore Development</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3><span><img src="images/services/offshore-development.png" alt="img" width="24"
+                                height="24"></span> Offshore Development</h3>
+                          <ul>
+                            <li><a href="#">App Development</a></li>
+                            <li><a href="#">UI / UX</a></li>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Offshore Development</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3><span><img src="images/services/dedicated-developers.png" alt="img" width="24"
+                                height="24"></span> Dedicated Developers</h3>
+                          <ul>
+                            <li><a href="#">App Development</a></li>
+                            <li><a href="#">UI / UX</a></li>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Offshore Development</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3><span><img src="images/services/devops-development.png" alt="img" width="24"
+                                height="24"></span> DevOps Development</h3>
+                          <ul>
+                            <li><a href="#">App Development</a></li>
+                            <li><a href="#">UI / UX</a></li>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">Offshore Development</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Portfolio</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="technologyDropdown" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Technology
+                  </a>
+                  <ul class="dropdown-menu large-dropdown technology-dropdown" aria-labelledby="technologyDropdown">
+                    <div class="row">
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3>Mobiles</h3>
+                          <ul>
+                            <li><a href="#"><span><img src="images/technology/ios.png" alt="icon" width="20px"
+                                    height="20px"></span> iOS</a></li>
+                            <li><a href="#"><span><img src="images/technology/android.png" alt="icon" width="20px"
+                                    height="20px"></span> Android</a></li>
+                            <li><a href="#"><span><img src="images/technology/react.png" alt="icon" width="20px"
+                                    height="20px"></span> React</a></li>
+                            <li><a href="#"><span><img src="images/technology/flutter.png" alt="icon" width="20px"
+                                    height="20px"></span> Flutter</a></li>
+                            <li><a href="#"><span><img src="images/technology/ionic.png" alt="icon" width="20px"
+                                    height="20px"></span> Ionic</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3>Front End</h3>
+                          <ul>
+                            <li><a href="#"><span><img src="images/technology/html-css.png" alt="icon" width="20px"
+                                    height="20px"></span> HTML/CSS</a></li>
+                            <li><a href="#"><span><img src="images/technology/bootstrap.png" alt="icon" width="20px"
+                                    height="20px"></span> Bootstrap</a></li>
+                            <li><a href="#"><span><img src="images/technology/material-ui.png" alt="icon" width="20px"
+                                    height="20px"></span> Material UI</a></li>
+                            <li><a href="#"><span><img src="images/technology/angular.png" alt="icon" width="20px"
+                                    height="20px"></span> Angular</a></li>
+                            <li><a href="#"><span><img src="images/technology/vue.png" alt="icon" width="20px"
+                                    height="20px"></span> Vue</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="large-menubox">
+                          <h3>Back End</h3>
+                          <ul>
+                            <li><a href="#"><span><img src="images/technology/node-js.png" alt="icon" width="20px"
+                                    height="20px"></span> Node JS</a></li>
+                            <li><a href="#"><span><img src="images/technology/python.png" alt="icon" width="20px"
+                                    height="20px"></span> Python</a></li>
+                            <li><a href="#"><span><img src="images/technology/django.png" alt="icon" width="20px"
+                                    height="20px"></span> Django</a></li>
+                            <li><a href="#"><span><img src="images/technology/ruby-on-rails.png" alt="icon" width="20px"
+                                    height="20px"></span> Rails</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Blogs</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link nav-link-btn" href="contact.php">Hire best Team<i class="icon-right ml-5"></i></a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <!-- Header End -->
